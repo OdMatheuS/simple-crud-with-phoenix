1badde4f55ffae9c90c3e4b40be4cce494bae318
Matheus <matheusnagot@hotmail.com>
2022-04-19 10:50:28 -0300

    feat: logic authenticating account with password

--- lib/bank_concept_web/auth/guardian.ex
+++ lib/bank_concept_web/auth/guardian.ex
@@ -1,5 +1,6 @@
 defmodule BankConceptWeb.Auth.Guardian do
   use Guardian, otp_app: :bank_concept
+  alias BankConcept.{Account, Repo}
 
   def subject_for_token(account, _claims) do
     sub = to_string(account.id)
@@ -10,5 +11,24 @@ defmodule BankConceptWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> BankConcept.get_account()
+  end
+
+  def authenticate(%{"id" => account_id, "password" => password}) do
+    case Repo.get(Account, account_id) do
+      nil -> {:error, "Account not found!"}
+      account -> validate_password(account, password)
+    end
+  end
+
+  defp validate_password(%Account{password_hash: password_hash} = account, password) do
+    case Argon2.verify_pass(password, password_hash) do
+      true -> create_token(account)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  defp create_token(account) do
+    {:ok, token, _claims} = encode_and_sign(account)
+    {:ok, token}
   end
 end
