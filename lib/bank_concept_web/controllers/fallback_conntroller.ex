9e8896782a1c348d9e33dc4335a1aa06fba7306f
Matheus <matheusnagot@hotmail.com>
2022-04-19 11:49:22 -0300

    feat:improving status and error msg not authorized

--- lib/bank_concept_web/controllers/fallback_conntroller.ex
+++ lib/bank_concept_web/controllers/fallback_conntroller.ex
@@ -1,6 +1,13 @@
 defmodule BankConceptWeb.FallbackController do
   use BankConceptWeb, :controller
 
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(BankConceptWeb.ErrorView)
+    |> render("401.json", message: "Account unauthorized")
+  end
+
   def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
